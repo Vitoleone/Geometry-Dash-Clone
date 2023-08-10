0e7086afa7e8f4f39d28f0aa3d804fb865899d15
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-08-11 00:29:23 +0300

    movement adjustment
    on ground detection changed to detecting with raycast.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,36 +6,73 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+  enum MovementType
+  {
+    Normal,
+    Rocket
+  }
+  [Header("Attributes")]
   [SerializeField] private float moveSpeed;
   [SerializeField] private float jumpPower;
+  [SerializeField] private LayerMask layerMask;
+  
   private Rigidbody2D _playerRb;
+  private Collider2D _playerCollider2D;
   private bool _onGround = true;
+  private MovementType _movementType;
 
   private void Awake()
   {
     _playerRb = GetComponent<Rigidbody2D>();
+    _playerCollider2D = GetComponent<Collider2D>();
   }
-  
-  private void Update()
+
+  private void FixedUpdate()
   {
-    gameObject.transform.position += transform.right * (moveSpeed * Time.deltaTime);
-    if (Input.GetMouseButton(0) && _onGround)
+    RaycastHit2D hit2D = Physics2D.Raycast(transform.position,Vector2.down * 0.52f,0.52f,layerMask);
+    if (!(hit2D.collider == null))
     {
-      Jump();
+      Debug.Log(hit2D.collider.name);
+      OnGround();
+    }
+    else
+    {
+      NotOnGround();
     }
   }
-  private void Jump()
+
+  private void Update()
+  {
+    if (_movementType == MovementType.Normal)
+    {
+      gameObject.transform.position += transform.right * (moveSpeed * Time.deltaTime);
+      if (Input.GetMouseButton(0) && _onGround)
+      {
+        NormalJump();
+      }
+    }
+    
+  }
+  private void NormalJump()
   {
     NotOnGround();
     _playerRb.AddForce(transform.up*jumpPower,ForceMode2D.Impulse);
   }
   public void OnGround()
   {
-    _onGround = true;
+    if (_playerRb.velocity.y <= 0)
+    {
+      _onGround = true;  
+    }
+    
   }
   public void NotOnGround()
   {
     _onGround = false;
   }
-  
+
+  private void OnDrawGizmos()
+  {
+    Debug.DrawRay(transform.position,Vector2.down * 0.52f,Color.black);
+  }
 }
